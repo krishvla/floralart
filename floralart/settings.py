5f0169379ce8d74e5b988453ae46542cb664d972
vamsi PythonDev <pothalavamsi@gmail.com>
2025-01-12 10:31:52 +0000

    pythonanywhere commit

--- floralart/settings.py
+++ floralart/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-y@f)u^lp$ck_vy7@#3297271s_9(c+ps3jl+989zi-vvl_tfqc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["floralart.pythonanywhere.com"]
 
 
 # Application definition
@@ -122,9 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
